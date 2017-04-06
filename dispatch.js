62316a95bfd390a4dc21a3e533218e4e5fc22155
Ryan <eating.911@gmail.com>
2017-04-06 10:14:23 +0800

    update

--- dispatch.js
+++ dispatch.js
@@ -1,6 +1,8 @@
 // dispatch.js
 const egg = require('egg')
+const workers = Number(process.argv[2] || require('os').cpus().length)
 egg.startCluster({
+  workers,
   baseDir: __dirname,
-  port: process.env.PORT || 7002 // default to 7002
+  port: process.env.PORT || 7002, // default to 7002
 })
